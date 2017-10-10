f3cf3b3edef29ec80aafb29564ab86037c014850
Tobias Simperl <simperl.tobias@gmail.com>
2017-10-11 00:35:51 +0200

    Initialisierung code snips c

---
+++ hexdump_main.c
@@ -0,0 +1,59 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <ctype.h>
+#define colnum  16
+
+static void hexdump(const void * memory, size_t bytes);
+ 
+int main(void) {
+  unsigned char ascii[256];
+  int i;
+  
+  for (i = 0; i < sizeof ascii; ++i)
+    ascii[i] = i;
+  hexdump(ascii, sizeof ascii);
+  return 0;
+  }
+
+static void hexdump (const void* data, size_t size){
+  const unsigned char * p, * q;
+  int i;
+  p = data;
+  
+  while (size)
+  {
+    q = p;
+    printf("%p: ", (void *) p);
+    
+    for (i = 0; i < colnum && size; ++i)
+    {
+      printf("%02X ", *p);
+      ++p;
+      --size;
+    }
+    size += i;
+    while (i < colnum)
+    {
+      printf("XX ");
+      ++i;
+    }
+  printf("| ");
+  
+  p=q;
+  
+  for (i = 0; i < colnum && size; ++i)
+  {
+    printf("%c", isprint(*p) && !isspace(*p) ? *p : ' ');
+    ++p;
+    --size;
+  }
+  while (i < colnum)
+  {
+    printf("");
+    ++i;
+  }
+  printf(" |\n");
+  
+}    
+return;
+}
